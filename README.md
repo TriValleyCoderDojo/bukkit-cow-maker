77a9ade01f341f3fe898a2322c4d186a3bb5d371
Bob Freitas <bob.e.freitas@gmail.com>
2014-05-25 16:45:16 -0700

    Initial checkin of code

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ bukkit-cow-maker
 ================
 
 Bukkit plugin to create cows with a command
+
+This is learning Bukkit plugin that will spawn ten cow with the command /cowabunga.
+It was adapted from an original plugin done by Joe Dean with the Campbell CoderDojo
+https://github.com/joedean/bukkit-plugins
+
---
+++ src/main/java/org/tvcd/cowabunga/Cowabunga.java
@@ -0,0 +1,41 @@
+package org.tvcd.cowabunga;
+
+import java.util.logging.Logger;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Cow;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Cowabunga extends JavaPlugin {
+	public static Logger log = Logger.getLogger("Minecraft");
+	@Override
+	public void onEnable() {
+		log.info("[Cowabunga] Start up.");
+	}
+	public void onReload() {
+		log.info("[Cowabunga] Server reloaded.");
+	}
+	@Override
+	public void onDisable() {
+		log.info("[Cowabunga] Server stopping.");
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command,
+			String commandLabel, String[] args) {
+		if (commandLabel.equalsIgnoreCase("cowabunga")) {
+			if (sender instanceof Player) {
+				Player me = (Player)sender;
+				// spawn ten cows where you are standing
+				Location location = me.getLocation();
+				for (int i = 0; i < 10 ; i++) {
+					me.getWorld().spawn(location, Cow.class);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+}
